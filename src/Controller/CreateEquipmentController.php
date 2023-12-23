b34675b1fea7f15e4369ef1bdbaa091b66c49586
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-23 10:55:32 +0100

    add the DB conection

--- src/Controller/CreateEquipmentController.php
+++ src/Controller/CreateEquipmentController.php
@@ -3,16 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CreateEquipmentController extends AbstractController
 {
     #[Route('/create/equipment', name: 'app_create_equipment')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createFormBuilder()
+            ->add('name')
+            ->add('description')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            // Log the data to the console (you can replace this with any logging mechanism)
+            $this->addFlash('success', 'Equipment created successfully!');
+            dump($data); // This will be logged to the console
+        }
+
         return $this->render('create_equipment/index.html.twig', [
-            'controller_name' => 'CreateEquipmentController',
+            'form' => $form->createView(),
         ]);
     }
 }

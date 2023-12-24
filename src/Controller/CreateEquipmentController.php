f54abe8f2d05e90b19050e3e07f5be58bd3d342d
sheimazb <zbedichaima@gmail.com>
2023-12-25 00:03:03 +0100

    crud equipment

--- src/Controller/CreateEquipmentController.php
+++ src/Controller/CreateEquipmentController.php
@@ -1,5 +1,6 @@
 <?php
 // src/Controller/CreateEquipmentController.php
+// src/Controller/CreateEquipmentController.php
 
 namespace App\Controller;
 
@@ -9,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Equipments;
+use App\Repository\EquipmentsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class CreateEquipmentController extends AbstractController
@@ -29,28 +31,48 @@ class CreateEquipmentController extends AbstractController
             // Persist the entity
             $entityManager->persist($equipment);
             $entityManager->flush();
-
-            return new Response('Saved new equipment with id ' . $equipment->getId());
+            $equipments = $entityManager->getRepository(Equipments::class)->findAll();
+        
         }
-
-        return $this->render('create_equipment/index.html.twig');
-    }
-
-    #[Route('/fetch-equipments', name: 'fetch_equipments')]
-    public function fetchEquipments(EntityManagerInterface $entityManager): Response
-    {
-        // Get the Doctrine EntityManager
-        $entityManager = $this->getDoctrine()->getManager();
-
-        // Fetch data from the Equipments entity
         $equipments = $entityManager->getRepository(Equipments::class)->findAll();
 
-        // You can also use custom queries, for example:
-        // $equipments = $entityManager->createQuery('SELECT e FROM App\Entity\Equipments e')->getResult();
+        return $this->render('create_equipment/index.html.twig', ['equipments' => $equipments]);
+    }
+    #[Route('/delete/{id}', name: 'equipment_delete')]
+    public function delete(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $equipment = $entityManager->getRepository(Equipments::class)->find($id);
 
-        return $this->render('your_template.html.twig', [
-            'equipments' => $equipments,
-        ]);
+        $entityManager->remove($equipment);
+        $entityManager->flush();
+        $this->addFlash(
+            'warning',
+            'Produit supprime avec succes...'
+        );
+
+        return $this->redirectToRoute('create_equipment');
+    }
+
+    #[Route('/equipment/update/{id}', name: 'app_equipment_update')]
+    public function update($id,Request $request, EquipmentsRepository $repos,EntityManagerInterface $em): Response
+    {
+
+        $equipment = $repos->find($id);
+        if (!$equipment) {
+            throw $this->createNotFoundException('Equipment not found');
+        }
+
+
+        $name = $request->request->get('equipment');
+        $description = $request->request->get('description');
+        $equipment->setName($name);
+        $equipment->setDescription($description);
+
+
+     
+       $em->persist($equipment);
+       $em->flush(); 
+        
+        return $this->redirectToRoute('create_equipment');
     }
 }
-

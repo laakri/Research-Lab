8d0053b4edfc877ce7124b141d48cbc32f4acdb5
sheimazb <zbedichaima@gmail.com>
2023-12-24 09:49:40 +0100

    changing ligin route and create equipment entu

---
+++ src/Entity/Equipments.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipmentsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EquipmentsRepository::class)]
+class Equipments
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?bool $disponibility = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $chercheurId = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $projectId = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function isDisponibility(): ?bool
+    {
+        return $this->disponibility;
+    }
+
+    public function setDisponibility(bool $disponibility): static
+    {
+        $this->disponibility = $disponibility;
+
+        return $this;
+    }
+
+    public function getChercheurId(): ?int
+    {
+        return $this->chercheurId;
+    }
+
+    public function setChercheurId(?int $chercheurId): static
+    {
+        $this->chercheurId = $chercheurId;
+
+        return $this;
+    }
+
+    public function getProjectId(): ?int
+    {
+        return $this->projectId;
+    }
+
+    public function setProjectId(?int $projectId): static
+    {
+        $this->projectId = $projectId;
+
+        return $this;
+    }
+}
